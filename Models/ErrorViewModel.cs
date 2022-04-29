3b57bab6792e54bb53fb77431978a7e67334259e
= <=>
2022-04-29 19:38:48 -0300

    Fix: N-Tier Architecture

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BulltyBookWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
